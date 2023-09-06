ef47b1b2992ded2a9bf1d14695f515c6a31b45c8
Mac May <tmay@cogentco.com>
2023-09-06 13:24:41 -0400

    Made script setup

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,14 +1,18 @@
-<script>
+<script setup>
   // import mechanics from '../services/mechanics.js'
   function rollDie() {
     return 1
   }
-  let number = 1
+  let number = ref(rollDie())
+  const objeOfAttrs = { // bind multiple attributes to an element at once
+    id: 'container',
+    class: 'wrapper'
+  }
 </script>
 
 <template>
   <main>
-    <span>Hello, Roshar</span>
-    <span> Something{{ this.number }}</span>
+    <span @click="">Hello, Roshar</span>
+    <span v-bind="objeOfAttrs">Something {{ number }}</span>
   </main>
 </template>
