6c356d8f88b1ee2d95c68484dd325e222e083eb2
Mac May <tmcpbmay@gmail.com>
2023-09-06 22:23:09 -0400

    Tested new format

--- src/views/TheMap.vue
+++ src/views/TheMap.vue
@@ -1,22 +1,42 @@
 <script setup>
-  import { ref, computed } from 'vue'
-  // import mechanics from '../services/mechanics.js'
-  function rollDie() {
-    return 1
-  }
-  let number = ref(rollDie())
+  import { computed } from 'vue'
+  import { diceSet } from '../services/mechanics.js'
+
+  let results = diceSet(3, 6)
+
   const objeOfAttrs = { // bind multiple attributes to an element at once
     id: 'container',
     class: 'wrapper'
   }
+  
   const display = computed(() => {
-    return number.value > 0 ? 1 : 0
+    return results
   })
+/**
+Remember this for when you need lifecycle stuff
+
+var vm = new Vue({
+  data(){
+    return{
+      myComputed: '',
+      computedData: 'Hello World'
+    }
+  },
+  created() {
+    this.myComputed = this.computedData;
+  },
+  watch: {
+    computedData() {
+      this.myComputed = this.computedData;
+    }
+  }
+});
+*/
 </script>
 
 <template>
   <main>
-    <span>Hello, Roshar</span><!-- @onclick  -->
+    <span>Hello, Roshar </span><!-- @onclick  -->
     <span v-bind="objeOfAttrs">Something {{ display }}</span>
   </main>
 </template>
