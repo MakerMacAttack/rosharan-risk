7f7e79a2279c0545b5328aee08f2b7144248baab
Mac May <tmcpbmay@gmail.com>
2023-09-13 08:53:40 -0400

    add ability to build deck from list of territories and shuffle

--- src/services/cards.js
+++ src/services/cards.js
@@ -1,15 +1,3 @@
-function shuffle(deck) {
-  let newDeck = []
-  let count = 0
-  while (deck.length > 0) {
-    count = Math.floor(Math.random() * (deck.length - 1))
-    newDeck.push(deck.splice(count, 1)[0])
-  }
-  return newDeck
-  }
-  
-shuffle([])
-
 class Card {
   constructor(territory, denomination) {
     this.territory = territory
@@ -19,7 +7,40 @@ class Card {
 
 class Deck {
   constructor(cards) {
-    this.cards = cards
+    this.denominations = [
+      "chip",
+      "mark",
+      "broam"
+    ]
+    this.cards = this.createDeck(cards)
+  }
+
+  shuffleSort() {
+    return 0.5 - Math.random()
+  }
+
+  createDeck(cards) {
+    let numberOfCards = cards.length
+    let evenSplit = Math.floor(numberOfCards / 3)
+    let remainder = numberOfCards % 3
+    let spheres = []
+    for (let i = 0; i < evenSplit; i++) {
+      spheres.push(...[0, 1, 2])
+    }
+    if (remainder > 0) {
+      spheres.push(0)
+    }
+    if (remainder > 1) {
+      spheres.push(1)
+    }
+    spheres.sort(this.shuffleSort)
+    return cards.map((territory, idx) => {
+      return new Card(territory, this.denominations[spheres[idx]])
+    })
+  }
+
+  shuffle() {
+    this.cards.sort(this.shuffleSort)
   }
 }
 
@@ -36,15 +57,44 @@ let alethkar = [
   "thanadal"
 ]
 
-let denominations = [ // Make this one of those unnecessarily complicated objects
-  "chip",
-  "mark",
-  "broam"
-]
+// let denominations = [ // Make this one of those unnecessarily complicated objects
+//   "chip",
+//   "mark",
+//   "broam"
+// ]
 
-let draw = new Deck(alethkar.map((highprincedom) => {
-  let sphere = Math.floor(Math.random() * 3)
-  return new Card(highprincedom, denominations[sphere])
-}))
+// let sphereList = []
 
-console.log(draw);
+// let evenSplit = Math.floor(alethkar.length / 3)
+
+// for (let i = 0; i < evenSplit; i++) {
+//   sphereList.push(0)
+//   sphereList.push(1)
+//   sphereList.push(2)
+// }
+
+// let remainder = alethkar.length % 3
+
+// if (remainder > 0) {
+//   sphereList.push(0)
+// }
+
+// if (remainder > 1) {
+//   sphereList.push(1)
+// }
+
+// sphereList.sort((a, b) => 0.5 - Math.random())
+
+// let draw = new Deck(alethkar.map((territory, idx) => {
+//   return new Card(territory, denominations[sphereList[idx]])
+// }))
+
+// console.log(draw);
+
+let draw = new Deck(alethkar)
+
+console.log(draw.cards);
+
+draw.shuffle()
+
+console.log(draw.cards);
