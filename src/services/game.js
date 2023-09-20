6bcbc9f9c51243d482501e5a5d2bd9e432cf0394
Mac May <tmay@cogentco.com>
2023-09-20 16:34:53 -0400

    tinker with game state

--- src/services/game.js
+++ src/services/game.js
@@ -3,16 +3,25 @@ export class Game {
       // What sort of things does the game need?
       this.players = players // make and import a Player object. I'm thinking start this as an array and then add players
       this.owner = players // make a player object out of owner's ID.
-      this.turn = 0
+      this.round = 0
       this.territories = [] // Store by zone? By territory?
+      this.currentPlayerIdx = 0
     }
 
     advanceTurn() { // Do I also need a separate method to advance round?
-
+      if (this.currentPlayerIdx === this.players.length - 1){
+        this.round++
+        this.currentPlayerIdx = 0
+      } else {
+        this.currentPlayerIdx++
+      }
     }
 
     startGame() {
-
+      //randomize players
+      //advance round to 1
+      //no more adding or removing players
+      //make it one player's turn
     }
 
     //Do I need a method to add players, or just this.players.push()?
