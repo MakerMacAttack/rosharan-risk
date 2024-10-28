539d32017eb75e2c8bf350118e10d1fcdb485ab6
Mac <tmcpbmay@gmail.com>
2024-10-28 08:21:20 -0400

    Starting to add die test to map page

--- src/views/TheMap.vue
+++ src/views/TheMap.vue
@@ -5,6 +5,23 @@
 <template>
   <main>
     <img src="../assets/Map First Draft.png" alt="map of Roshar">
+    <div class="player-area">
+      <form id="die-test">
+        <button type="submit">Click Here</button>
+        <div>Attacker</div>
+        <select id="attacker" name="attacker">
+          <option value=1>1</option>
+          <option value=2>2</option>
+          <option value=3>3</option>
+        </select>
+        <div>Defender</div>
+        <select id="defender" name="defender">
+          <option value=1>1</option>
+          <option value=2>2</option>
+        </select>
+      </form>
+      <!-- Okay, here I need the output. So it should default saying 0, and then when I submit the form above the output should be displayed here. -->
+    </div>
   </main>
 </template>
 
